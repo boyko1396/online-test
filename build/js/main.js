b21868b7419f5dfcced1866dadca63efb7dd0bf2
Vasya <boyko1396@gmail.com>
2022-10-26 12:28:07 +0300

    edit form test

--- build/js/main.js
+++ build/js/main.js
@@ -1,4 +1,35 @@
-$(document).ready(function() {
+$(document).ready(function() {    
+    // test pager
+    $('.tasks-field-wrap').click(function (e) {
+        var screenTest = $('.tab-pane.active');
+        screenTestNext = screenTest.next();
+        screenTestPrev = screenTest.prev();
+        $('.tab-pane.active .tasks-field-wrap').removeClass('active-item');
+        $(this).addClass('active-item');
+        if (screenTest.find('.tasks-field-wrap').hasClass('active-item')) {
+            screenTest.find('.main-form-b__footer-btn-next').removeClass('disabled');
+            return false;
+        }
+    });
+
+    $('.main-form-b__footer-btn-next').click(function (e) {
+        var screenTest = $('.tab-pane.active');
+        screenTestNext = screenTest.next();
+        screenTestPrev = screenTest.prev();
+        if (screenTest.find('.tasks-field-wrap').hasClass('active-item')) {
+            screenTest.removeClass('active');
+            screenTestNext.addClass('active');
+        }
+    });
+
+    $('.main-form-b__footer-btn-prev').click(function (e) {
+        var screenTest = $('.tab-pane.active');
+        screenTestNext = screenTest.next();
+        screenTestPrev = screenTest.prev();
+        screenTest.removeClass('active');
+        screenTestPrev.addClass('active');
+    });
+
     // modal sms
     $('.js-modal-sms-init').on('click', function (e) {
         $('body').toggleClass('is-modal-opened');
